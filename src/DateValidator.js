41a7ed47febbf101be56bea45210fd75c6e9b654
Meriam Ben Sassi <meriam.bensassi@yelloan.com>
2017-08-31 14:48:01 +0200

    Update

--- src/DateValidator.js
+++ src/DateValidator.js
@@ -4,7 +4,7 @@ var hd = new Holidays();
 
 class DateValidator {
 
-  constructor(beginingTime, endingTime, country, state, region, opts) {
+  constructor(exludeWeekend, exludeHolidays, beginingTime, endingTime, country, state, region, opts) {
     hd.init(country, state, region, opts);
 
     let validBeginingTime = moment(beginingTime, 'HH:mm').isValid();
@@ -17,9 +17,12 @@ class DateValidator {
 
     this.beginingTime = beginingTime;
     this.endingTime = endingTime;
+    this.exludeWeekend = exludeWeekend;
+    this.exludeHolidays = exludeHolidays ;
   }
 
   isValidTime(time) {
+
     if (!moment(time, 'HH:mm').isValid()) {
       throw new Error('INVALID_PARAMETERS');
     }
@@ -27,6 +30,7 @@ class DateValidator {
     let timeAfterBegining =  moment(time, 'HH:mm').isSameOrAfter(moment(this.beginingTime, 'HH:mm'));
     let timeBeforeEnding = moment(time, 'HH:mm').isSameOrBefore(moment(this.endingTime, 'HH:mm'));
     let isValidTime = timeAfterBegining && timeBeforeEnding;
+
     return (isValidTime);
   }
 
@@ -35,10 +39,39 @@ class DateValidator {
       throw new Error('INVALID_PARAMETERS');
     }
     let time = moment(date).format('HH:mm') ;
-    if (!hd.isHoliday(date) && this.isValidTime(time)) {
-      return true;
+    let isValidTime = this.isValidTime(time);
+    let isHoliday = hd.isHoliday(date);
+    let isWeekend = this.isWeekend(date);
+    let isValidDate = true;
+
+    if (this.exludeHolidays && isHoliday) {
+      isValidDate = false;
     }
+
+    if (this.exludeWeekend && isWeekend) {
+      isValidDate = false;
+    }
+
+    if (!isValidTime) {
+      isValidDate = false;
+    }
+    return isValidDate;
+  }
+
+  isWeekend (date) {
+
+    if (!moment(date).isValid()) {
+      throw new Error('INVALID_PARAMETERS');
+    }
+
+    let isoWeekDay = moment(date).isoWeekday();
+
+    if (isoWeekDay === 7 || isoWeekDay === 6) {
+      return true
+    }
+
     return false;
+
   }
 
   nextValidDate(date) {
@@ -47,16 +80,24 @@ class DateValidator {
       throw new Error('INVALID_PARAMETERS');
     }
 
+
     if (this.isValidDate(date)) {
       return date;
+    }
+
+    let hours = moment(this.beginingTime, 'HH:mm').hours();
+    let minutes = moment(this.beginingTime, 'HH:mm').minutes();
+
+    if (moment(date, 'HH:mm').isBefore(moment(this.beginingTime, 'HH:mm'))) {
+      date = moment(date).hours(hours).format();
+      date = moment(date).minutes(minutes).format();
     }
 
     while (!this.isValidDate(date)) {
       date = moment(date).add(1, 'days').format();
     }
 
-    let hours = moment(this.beginingTime, 'HH:mm').hours();
-    let minutes = moment(this.beginingTime, 'HH:mm').minutes();
+
     date = moment(date).hours(hours).format();
     date = moment(date).minutes(minutes).format();
 
--- test/dateValidator.test.js
+++ test/dateValidator.test.js
@@ -10,7 +10,7 @@ describe('The DateValidator', function() {
       let beginingTime = 'cc:w';
       let endingTime = '19:00';
       should(() => {
-        let dateValidator = new DateValidator(beginingTime, endingTime, country);
+        let dateValidator = new DateValidator(true, true, beginingTime, endingTime, country);
       }).throw('INVALID_PARAMETERS');
     });
 
@@ -19,7 +19,7 @@ describe('The DateValidator', function() {
       let beginingTime = '19:00';
       let endingTime = '8:00';
       should(() => {
-        let dateValidator = new DateValidator(beginingTime, endingTime, country);
+        let dateValidator = new DateValidator(true, true, beginingTime, endingTime, country);
       }).throw('INVALID_PARAMETERS');
 
     });
@@ -28,7 +28,7 @@ describe('The DateValidator', function() {
       let country = 'FR';
       let beginingTime = '8:00';
       let endingTime = '19:00';
-      let dateValidator = new DateValidator(beginingTime, endingTime, country);
+      let dateValidator = new DateValidator(true, true, beginingTime, endingTime, country);
       should(dateValidator.beginingTime).eql('8:00');
       should(dateValidator.endingTime).eql('19:00');
     });
@@ -39,7 +39,7 @@ describe('The DateValidator', function() {
       let country = 'FR';
       let beginingTime = '8:00';
       let endingTime = '19:00';
-      let dateValidator = new DateValidator(beginingTime, endingTime, country);
+      let dateValidator = new DateValidator(true, true, beginingTime, endingTime, country);
       should(dateValidator.isValidTime('10:00')).eql(true);
 
     });
@@ -48,7 +48,7 @@ describe('The DateValidator', function() {
       let country = 'FR';
       let beginingTime = '8:00';
       let endingTime = '19:00';
-      let dateValidator = new DateValidator(beginingTime, endingTime, country);
+      let dateValidator = new DateValidator(true, true, beginingTime, endingTime, country);
       should(dateValidator.isValidTime('02:00')).eql(false);
 
     });
@@ -57,22 +57,61 @@ describe('The DateValidator', function() {
       let country = 'FR';
       let beginingTime = '8:00';
       let endingTime = '19:00';
-      let dateValidator = new DateValidator(beginingTime, endingTime, country);
+      let dateValidator = new DateValidator(true, true, beginingTime, endingTime, country);
       should(()=> {
         dateValidator.isValidTime('invalidTime');
       }).throw('INVALID_PARAMETERS');
     });
   });
 
+  describe('Method isWeekend', function() {
+    it('should return true if the given date is a weekend day', function() {
+      let country = 'FR';
+      let beginingTime = '8:00';
+      let endingTime = '19:00';
+      let date = new Date('2017-09-02');
+      let dateValidator = new DateValidator(true, true, beginingTime, endingTime, country);
+      should(dateValidator.isWeekend(date)).eql(true);
+    });
+
+    it('should return false if the given time is not a weekend day', function() {
+      let country = 'FR';
+      let beginingTime = '8:00';
+      let endingTime = '19:00';
+      let date = new Date('2017-09-04');
+      let dateValidator = new DateValidator(true, true, beginingTime, endingTime, country);
+      should(dateValidator.isWeekend(date)).eql(false);
+
+    });
+
+    it('should throw an error if the given time is invalid', function() {
+      let country = 'FR';
+      let beginingTime = '8:00';
+      let endingTime = '19:00';
+      let dateValidator = new DateValidator(true, true, beginingTime, endingTime, country);
+      should(()=> {
+        dateValidator.isWeekend('invalidTime');
+      }).throw('INVALID_PARAMETERS');
+    });
+  });
 
   describe('Method isValidDate', function() {
-    it('should return false if the given date is a holiday', function() {
+    it('should return false if the given date is a holiday and excludeHoliday is set to true', function() {
       let country = 'FR';
       let beginingTime = '8:00';
       let endingTime = '19:00';
       let date = new Date('2017-01-01');
-      let dateValidator = new DateValidator(beginingTime, endingTime, country);
+      let dateValidator = new DateValidator(true, true, beginingTime, endingTime, country);
       should(dateValidator.isValidDate(date)).eql(false);
+    });
+
+    it('should return true if the given date is a holiday and excludeHoliday is set to false', function() {
+      let country = 'FR';
+      let beginingTime = '8:00';
+      let endingTime = '19:00';
+      let date = new Date('2018-01-01 12:00:00 GMT+0000');
+      let dateValidator = new DateValidator(true, false, beginingTime, endingTime, country);
+      should(dateValidator.isValidDate(date)).eql(true);
     });
 
     it('should return false if the given time is not in the interval', function() {
@@ -80,7 +119,7 @@ describe('The DateValidator', function() {
       let beginingTime = '8:00';
       let endingTime = '19:00';
       let date = new Date('2017-08-16 00:00:00 GMT+0000');
-      let dateValidator = new DateValidator(beginingTime, endingTime, country);
+      let dateValidator = new DateValidator(true, true, beginingTime, endingTime, country);
       should(dateValidator.isValidDate(date)).eql(false);
     });
 
@@ -89,7 +128,25 @@ describe('The DateValidator', function() {
       let beginingTime = '8:00';
       let endingTime = '19:00';
       let date = new Date('2017-08-16 09:00:00 GMT+0000');
-      let dateValidator = new DateValidator(beginingTime, endingTime, country);
+      let dateValidator = new DateValidator(true, true, beginingTime, endingTime, country);
+      should(dateValidator.isValidDate(date)).eql(true);
+    });
+
+    it('should return false if the date is a week end and exclude weekend is set to true', function() {
+      let country = 'FR';
+      let beginingTime = '8:00';
+      let endingTime = '19:00';
+      let date = new Date('2017-09-03 09:00:00 GMT+0000');
+      let dateValidator = new DateValidator(true, true, beginingTime, endingTime, country);
+      should(dateValidator.isValidDate(date)).eql(false);
+    });
+
+    it('should return true if the date is a week end and exclude weekend is set to false', function() {
+      let country = 'FR';
+      let beginingTime = '8:00';
+      let endingTime = '19:00';
+      let date = new Date('2017-09-03 09:00:00 GMT+0000');
+      let dateValidator = new DateValidator(false, true, beginingTime, endingTime, country);
       should(dateValidator.isValidDate(date)).eql(true);
     });
 
@@ -97,7 +154,7 @@ describe('The DateValidator', function() {
       let country = 'FR';
       let beginingTime = '8:00';
       let endingTime = '19:00';
-      let dateValidator = new DateValidator(beginingTime, endingTime, country);
+      let dateValidator = new DateValidator(true, true, beginingTime, endingTime, country);
       should(function () {
         dateValidator.isValidDate('invalidDate');
       }).throw('INVALID_PARAMETERS');
@@ -111,7 +168,7 @@ describe('The DateValidator', function() {
       let beginingTime = '8:00';
       let endingTime = '19:00';
       let date = new Date('2017-01-02 12:00');
-      let dateValidator = new DateValidator(beginingTime, endingTime, country);
+      let dateValidator = new DateValidator(true, true, beginingTime, endingTime, country);
       should(moment(dateValidator.nextValidDate(date)).isSame(moment(date))
     ).eql(true);
     });
@@ -122,20 +179,34 @@ describe('The DateValidator', function() {
       let endingTime = '19:00';
       let date = new Date('2017-01-01 12:00');
       let nextDate = new Date('2017-01-02 8:00');
-      let dateValidator = new DateValidator(beginingTime, endingTime, country);
+      let dateValidator = new DateValidator(true, true, beginingTime, endingTime, country);
       should(moment(dateValidator.nextValidDate(date)).isSame(moment(nextDate))
     ).eql(true);
     });
+
+    it('should return the next valid date if it is not a valid one (invalid time)', function() {
+      let country = 'FR';
+      let beginingTime = '8:00';
+      let endingTime = '19:00';
+      let date = new Date('2017-01-02 07:00');
+      let nextDate = new Date('2017-01-02 8:00');
+      let dateValidator = new DateValidator(true, true, beginingTime, endingTime, country);
+      should(moment(dateValidator.nextValidDate(date)).isSame(moment(nextDate))
+    ).eql(true);
+    });
+
 
 
     it('should throw an error if the given date is invalid', function() {
       let country = 'FR';
       let beginingTime = '8:00';
       let endingTime = '19:00';
-      let dateValidator = new DateValidator(beginingTime, endingTime, country);
+      let dateValidator = new DateValidator(true, true, beginingTime, endingTime, country);
       should(()=> {
         dateValidator.nextValidDate('invalidDate');
       }).throw('INVALID_PARAMETERS');
     });
   });
+
+
 });
